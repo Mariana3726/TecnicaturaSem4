1eb64072c221643b1e7b8ba060c164d4aad3b563
Mariana Aguilera <marian5174@gmail.com>
2025-08-14 23:52:22 -0300

    Agregamos estilos

--- E-Commerce/client/js/index.js
+++ E-Commerce/client/js/index.js
@@ -2,7 +2,7 @@ const shopContent = document.getElementById("shopContent"); //busca en el html e
 
 productos.forEach((product) =>{ // con el forEach recorremos cada producto del array y lo guardamos en la variable product
     const content = document.createElement("div"); //creamos el elemento vacío
-    content.innerHTML =  `      //define el contenido html que acabamos de crear/ la comilla para insertar valores dinámicos
+    content.innerHTML =  ` 
     <img src="${product.img}">
     <h3>${product.productName}</h3>
     <p>${product.price} $</p>
