75c244f7867023aff4823da0adafd5e0ac53d589
Mariana Aguilera <marian5174@gmail.com>
2025-08-23 02:07:51 -0300

    Creamos el index.html

--- E-Commerce/client/js/index.js
+++ E-Commerce/client/js/index.js
@@ -1,4 +1,5 @@
 const shopContent = document.getElementById("shopContent"); //busca en el html el elemento con el ID shopContent
+const cart = []; //este es el carrito, un array vacío
 
 productos.forEach((product) =>{ // con el forEach recorremos cada producto del array y lo guardamos en la variable product
     const content = document.createElement("div"); //creamos el elemento vacío
@@ -8,4 +9,20 @@ productos.forEach((product) =>{ // con el forEach recorremos cada producto del a
     <p>${product.price} $</p>
      ` ;
     shopContent.append(content);
+
+    const buyButton = document.createElement("button"); //creamos el elemento
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", ()=>{ //agregamos evento
+        CaretPosition.push({
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,
+        })
+        console.log(cart)
+    })
 });
