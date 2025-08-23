21a9eb4ae2477cbedc0de1fa38d803b9cf9ec820
Mariana Aguilera <marian5174@gmail.com>
2025-08-23 00:03:15 -0300

    Decimal, Binario, Octal y Hexadecimal

---
+++ pythonProject/Sistemas_numeracion.py
@@ -0,0 +1,17 @@
+# Profundizando en los sistemas de numeración
+
+# sistema decimal
+a = 10
+print(f'a decimal: {a}')
+
+# sistema binario
+a = 0b1010
+print(f'a binario: {a}')
+
+# sistema octal
+a = 0o12
+print(f'a octal: {a}')
+
+# sistema hexadecimal
+a = 0xA
+print(f'a hexadecimal: {a}')
