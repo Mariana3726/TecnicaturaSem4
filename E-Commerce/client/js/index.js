2ba5ab1c85174df0e42945092c7c6b97da2de8c8
Mariana Aguilera <marian5174@gmail.com>
2025-08-26 23:27:33 -0300

    Configuramos las cantidades de los productos

--- E-Commerce/client/js/index.js
+++ E-Commerce/client/js/index.js
@@ -17,13 +17,23 @@ productos.forEach((product) =>{ // con el forEach recorremos cada producto del a
     content.append(buyButton);
 
     buyButton.addEventListener("click", ()=>{ //agregamos evento
-        cart.push({
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((prod) => {
+                if(prod.id === product.id){
+                    prod.quanty++;
+                }
+            });
+        }else{
+            cart.push({
             id: product.id,
             productName: product.productName,
             price: product.price,
             quanty: product.quanty,
             img: product.img,
-        })
-        console.log(cart)
-    })
+        });
+    }
+
+    });
 });
