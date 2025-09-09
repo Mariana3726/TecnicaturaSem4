110dd25bdabe08bb487dd33a91148165be94f140
Mariana Aguilera <marian5174@gmail.com>
2025-09-09 18:54:35 -0300

    Constructor bool

---
+++ Python/Clase02/tipo_bool.py
@@ -0,0 +1,11 @@
+
+# Bool contiene los valores de True y False
+# Los tipo numéricos,false para el 0 y true para los demás valores
+
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 15
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
