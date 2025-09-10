234cfb767cc0675393bab69411d172fa7fe5e540
Mariana Aguilera <marian5174@gmail.com>
2025-09-10 00:28:39 -0300

    Documentación -> doc String

---
+++ Python/Clase04/mi_clase.py
@@ -0,0 +1,21 @@
+
+
+class MiClase:
+    '''
+    este es un ejemplo de la documenyacion de esta clase.
+    Es solo para saber que aquí esta MiClase
+    '''
+    def __init__(self):
+        '''
+        aqui esta el metodo dunder init
+        este método es el inicio de esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        éste es el método creado por mi que recibe parámetros
+        Detectó los parámetros automátivamente
+        :param param1: este es el parámetro numero 1
+        :param param2: este es el parámetro número 2
+        :return: este es el valor de retorno
+        '''
