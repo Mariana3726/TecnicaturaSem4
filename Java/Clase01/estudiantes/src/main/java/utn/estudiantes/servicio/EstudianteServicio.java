e17d04d4d2f35b311bbd18f21cc759d00e5a7087
Mariana Aguilera <marian5174@gmail.com>
2025-09-11 17:28:33 -0300

    Agregamos los métodos listar, buscar, modificar, eliminar y salir

--- Java/Clase01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Clase01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -2,35 +2,34 @@ package utn.estudiantes.servicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes1;
 import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
 @Service
-public class EstudianteServicio implements IEstudianteServicio{
+public class EstudianteServicio implements IEstudianteServicio {
+
     @Autowired
     private EstudianteRepositorio estudianteRepositorio;
 
     @Override
-    public List<Estudiante> listarEstudiantes() {
-        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
-        return estudiantes;
+    public List<Estudiantes1> listarEstudiantes() {
+        return estudianteRepositorio.findAll();
     }
 
     @Override
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
-        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
-        return estudiante;
+    public Estudiantes1 buscarEstudiantePorId(Integer idEstudiante) {
+        return estudianteRepositorio.findById(idEstudiante).orElse(null);
     }
 
     @Override
-    public void guardarEstudiante(Estudiante estudiante) {
+    public void guardarEstudiante(Estudiantes1 estudiante) {
         estudianteRepositorio.save(estudiante);
     }
 
     @Override
-    public void eliminarEstudiante(Estudiante estudiante) {
+    public void eliminarEstudiante(Estudiantes1 estudiante) {
         estudianteRepositorio.delete(estudiante);
     }
 }
