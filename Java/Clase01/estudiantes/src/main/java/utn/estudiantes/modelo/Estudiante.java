fc98d9fec94a87365efaf7f4a0fe0b1af38f07d0
Mariana Aguilera <marian5174@gmail.com>
2025-09-11 12:00:47 -0300

    Creación del paquete modelo y la clase entidad Estudiante

---
+++ Java/Clase01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,26 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//boilerplate - Repetitivo
+@Data                          //para los getters y setters
+@NoArgsConstructor              //constructor vacío
+@AllArgsConstructor             //constructor con todos los argumentos
+@ToString
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
