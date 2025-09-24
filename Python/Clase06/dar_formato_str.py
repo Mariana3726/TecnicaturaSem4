458c2ae037089f12ae87e9ab11e44ab78af4519a
Mariana Aguilera <marian5174@gmail.com>
2025-09-23 23:48:05 -0300

    Método format

--- Python/Clase06/dar_formato_str.py
+++ Python/Clase06/dar_formato_str.py
@@ -14,5 +14,12 @@ edad = 19
 sueldo = 3000
 # mensaje_con_formato = 'Nombre {} edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
 
-mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+#mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+#print(mensaje)
+
+mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo)
+#print(mensaje)
+
+diccionario = {'nombre': 'Ivan', 'edad': 35, 'sueldo': 900000.00}
+mensaje = 'Nombre {dic[nombre]} Edad {dic[edad]} Sueldo {dic[sueldo]:.2f}'.format(dic=diccionario)
 print(mensaje)
