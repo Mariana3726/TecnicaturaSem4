5d843334e661fcfefdcde759d08e594254c23896
Mariana Aguilera <marian5174@gmail.com>
2025-09-23 23:31:45 -0300

    Uso del método format, utilizamos place holder

--- Python/Clase06/dar_formato_str.py
+++ Python/Clase06/dar_formato_str.py
@@ -3,4 +3,16 @@
 nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
-print(mensaje_con_formato)
+
+# Creamos una tupla
+persona = ('Carla', 'Gomez', 500000.00)
+mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f' #% persona #aqui le pasamos el objeto que es tupla
+print(mensaje_con_formato % persona)
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+# mensaje_con_formato = 'Nombre {} edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
