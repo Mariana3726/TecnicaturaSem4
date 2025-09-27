8c3ad85ae838d33c1b4fbafc985619e55084d695
Mariana Aguilera <marian5174@gmail.com>
2025-09-27 19:34:36 -0300

    Nuevo proyecto con pygame

---
+++ Python/Clase07/Juego_Tecnicatura/constantes.py
@@ -0,0 +1,13 @@
+import os
+
+# Dimension de la pantalla, el alto y el ancho
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+# Especificar los colores de la pantalla
+
+COLOR_LASER = (0,0, 255) #azul
+
+
+#Ruta a la carpeta assets
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), 'assets')
